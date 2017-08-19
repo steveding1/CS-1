993227d7897012083932ae2b6dfb9d3a4e24b434
steveding1 <ningding100@gmail.com>
2017-08-19 22:26:06 +1000

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,4 @@
+#CS-1 Task 2.1 - Chapter 2 Part 2 from Steve Ding
+num1 = int(input('Enter the 1st number'))
+num2 = int(input('Enter the 2nd number'))
+print ('The sum of the 2 numbers is ', num1+num2)
