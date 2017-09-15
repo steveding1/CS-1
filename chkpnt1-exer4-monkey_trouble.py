f8d257684dc8a9c483d9a527f53e62079881299a
steveding1 <ningding100@gmail.com>
2017-09-15 22:08:06 +1000

    Update chkpnt1-exer4-monkey_trouble.py

--- chkpnt1-exer4-monkey_trouble.py
+++ chkpnt1-exer4-monkey_trouble.py
@@ -1,15 +1,17 @@
 #! python3
 #CS-1 Checkpoint 1 - Exercise 4 - monkey_trouble
-import sys
 def monkey_trouble(monkey1,monkey2):
+    import sys
     try:
-        if (monkey1 and monkey2) == True:
-            return True
-        elif(monkey1 == False and monkey2 == False):
-            return True
+        if (monkey1 == 1 and monkey2 == 1):
+            print("True")
+        elif(monkey1 == 0 and monkey2 == 0):
+            print("True")
         else:
-            return False
-    
+            print("False")
     except NameError: 
-        print ("Error, please enter \"True\" or \"False")
+        print ("Error, please enter type 1 for true, 0 for false")
         sys.exit()
+monkey1flag = int(input("monkey1 smiling?(type 1 for true, 0 for false)"))
+monkey2flag = int(input("monkey2 smiling?(type 1 for true, 0 for false)"))
+monkey_trouble(monkey1flag,monkey2flag)
