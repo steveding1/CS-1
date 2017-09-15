3f53d6bf8c6de250eaa5cddf873786d276d82ad1
steveding1 <ningding100@gmail.com>
2017-09-15 22:18:44 +1000

    Update chkpnt1-exer5-sum_double.py

--- chkpnt1-exer5-sum_double.py
+++ chkpnt1-exer5-sum_double.py
@@ -1,16 +1,19 @@
 #! python3
 #CS-1 Checkpoint 1 - Exercise 5 - sum_double
+def sum_double(value1,value2):
+    v1 = int(value1)
+    v2 = int(value2)
+    if v1 == v2:
+        sum = (v1 + v2)*2
+    else:
+        sum = v1 + v2
+    print(sum)
+
 import sys
 try:
-    def sum_double(value1,value2):
-        v1 = int(value1)
-        v2 = int(value2)
-        if v1 == v2:
-            sum = (v1 + v2)*2
-        else:
-            sum = v1 + v2
-        print(sum)
-    
-except NameError: 
-    print ("Error, please enter 2 intergers")
+    v1 = int(input("plz input value1 = "))
+    v2 = int(input("plz input value2 = "))
+except ValueError: 
+    print ("plz input intergers only")
     sys.exit()
+sum_double(v1,v2)
