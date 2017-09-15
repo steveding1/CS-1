d23464952796ac3674c1ed4977ee327c96fb34ee
steveding1 <ningding100@gmail.com>
2017-09-15 21:32:00 +1000

    Update chkpnt1-exer3-sleep_in.py

--- chkpnt1-exer3-sleep_in.py
+++ chkpnt1-exer3-sleep_in.py
@@ -1,12 +1,14 @@
 #CS-1 Checkpoint 1 - Exercise 3 - sleep in
-try:
-    def sleep_in(weekday, vacation):
-        if weekday is False:
+weekdayflag = input("is it a weekday?(type true/false)")
+vacationflag = input("is it a vacation?(type true/false)")
+
+def sleep_in(weekday, vacation):
+    if weekday == 'false':
+        print ("True")
+    else:
+        if vacation == 'true':
             print ("True")
         else:
-            if vacation is True:
-                print ("True")
-            else:
-                print ("False")
-except NameError:
-    print ("Error, please enter true or false")
+            print ("False")
+
+sleep_in(weekdayflag,vacationflag)
