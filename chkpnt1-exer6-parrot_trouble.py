99d4660dfb46a46d68e320d2ebd6fc570b6c512e
steveding1 <ningding100@gmail.com>
2017-09-19 22:33:20 +1000

    Update chkpnt1-exer6-parrot_trouble.py

--- chkpnt1-exer6-parrot_trouble.py
+++ chkpnt1-exer6-parrot_trouble.py
@@ -1,15 +1,24 @@
 #! python3
-#CS-1 Checkpoint 1 - Exercise 5 - sum_double
+#CS-1 Checkpoint 1 - Exercise 6 - parrot_troubletry:
+def sum_double(boole,hour):
+    hour1 = int(hour)
+    if (boole and (hour1<7 or hour1>23) is True):
+        print ("True")
+    else:
+        print ("False")
+
 import sys
 try:
-    def sum_double(boole,hour):
-        hour1 = int(hour)
-        if (boole and (hour1<=7 or hour1>=23) is True):
-            print ("True")
-        else:
-            print ("False")
-            
-    
+    boole_flag = input("if the parrot is talking(input True if talking and False if not) ")
+    hour_flag = int(input("what hours is now? "))
 except ValueError:
     print ("Error, please enter a number as hour")
     sys.exit()
+if boole_flag is "True" or True:
+    boole_flag1 = True
+elif boole_flag is "False" or False:
+    boole_flag1 = False
+else:
+    print ("Error, please enter True or False as if the parrot is talking")
+    sys.exit()
+sum_double(boole_flag,hour_flag)
